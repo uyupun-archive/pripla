bc05ca36d5adac347a9b2e8dd56c0b1726b16ef6
Kazuki Tobita <kazukiti201@gmail.com>
2020-11-05 04:39:05 +0900

    Modify top

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,23 +1,15 @@
 <template>
-  <div>
+  <div class="container">
     <div class="background-image">
-      <div class="container">
-        <div class="area-title">
-          <img
-            class="mainlogo"
-            src="~/assets/logo/logo_white.png"
-            alt="ロゴ"
-            width="75"
-            height="75"
-          />
-          <h2 class="title">Pripla</h2>
-          <h4 class="subtitle">Princess & Prince date planning</h4>
-        </div>
+      <div class="logo-wrapper">
+        <img class="logo-image" src="~/assets/logo/logo_black.png" alt="ロゴ" />
+        <h2 class="logo-title">Pripla</h2>
+        <h4 class="logo-subtitle">Princess & Prince date planning</h4>
       </div>
     </div>
-    <div class="container">
-      <h5 class="explain">臨機応変なデートプランをシェア</h5>
-      <p class="explain-containt">
+    <div class="feature-wrapper">
+      <h5 class="feature-title">臨機応変なデートプランをシェア</h5>
+      <p class="feature-description">
         ほげほげほげほげほげほげほげほげほげほげ
         ほげほげほげほげほげほげほげほげほげほげ
         ほげほげほげほげほげほげほげほげほげほげ
@@ -26,9 +18,9 @@
         ほげほげほげほげほげほげほげほげほげほげ
         ほげほげほげほげほげほげほげほげほげほげ
       </p>
-      <div>
-        <button>さあ、はじめよう</button>
-      </div>
+    </div>
+    <div class="start-btn">
+      <button>さあ、はじめよう</button>
     </div>
   </div>
 </template>
@@ -38,40 +30,52 @@
 <style lang="scss" scoped>
 .background-image {
   height: 250px;
-  background-image: url(https://placehold.jp/375x250.png);
+  background-image: url(https://placehold.jp/400x250.png);
   background-position: center;
   background-repeat: no-repeat;
+  margin-bottom: 30px;
 }
 
-.container {
-  position: absolute;
-  text-align: center;
-}
-
-.area-title {
-  color: white;
-}
-
-.mainlogo {
-  margin: 70px auto 0;
-}
-
-.title {
-  font-size: 36px;
-  margin: 0 auto;
-}
-
-.subtitle {
-  font-size: 16px;
-  margin: 0 auto;
-}
-.explain {
-  font-size: 18px;
-
-  &-containt {
-    max-width: 350px;
-    font-size: 14px;
-    margin: 0 auto 30px;
+.logo {
+  &-wrapper {
+    padding-top: 60px;
+    text-align: center;
   }
+
+  &-image {
+    width: 80px;
+    height: 80px;
+  }
+
+  &-title {
+    margin: 0;
+    font-size: 36px;
+  }
+
+  &-subtitle {
+    margin: 0;
+    font-size: 16px;
+  }
+}
+
+.feature {
+  &-wrapper {
+    text-align: center;
+  }
+
+  &-title {
+    margin: 0 0 30px 0;
+    font-size: 18px;
+  }
+
+  &-description {
+    margin: 0 0 30px 0;
+    width: 100%;
+    font-size: 14px;
+  }
+}
+
+.start-btn {
+  text-align: center;
 }
 </style>
