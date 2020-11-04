a19b0a37ebb6835335bd2249dd1f3f0b729ded34
Kazuki Tobita <kazukiti201@gmail.com>
2020-11-05 02:38:53 +0900

    Add favicon

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8" />
+        <title>Pripla API</title>
+        <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}" />
+    </head>
+    <body>
+        <h1>Pripla API</h1>
+    </body>
+</html>
