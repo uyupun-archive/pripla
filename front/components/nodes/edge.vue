fa01e384a85a50c87fe5e105810e732db0dc1a0b
suzuking1125 <b7165@oic.jp>
2020-11-06 07:57:41 +0900

    create edgenode

---
+++ front/components/nodes/edge.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="edgenode" :type="string">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.edgenode {
+  width: 200px;
+  padding: 13px 20px;
+  border-radius: 50px;
+  display: inline-block;
+  text-align: center;
+  color: $white;
+  font-size: 14px;
+  line-height: 1;
+  background: $pink;
+  outline: none;
+  box-sizing: border-box;
+  border: none;
+}
+</style>
