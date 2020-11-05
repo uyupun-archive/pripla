3c7640906e8929a7acee844061da0d6c2ff73422
Kazuki Tobita <kazukiti201@gmail.com>
2020-11-06 04:42:03 +0900

    Modify tl api (add optional params)

--- app/Http/Controllers/Api/V0/TimelineController.php
+++ app/Http/Controllers/Api/V0/TimelineController.php
@@ -5,18 +5,25 @@ namespace App\Http\Controllers\Api\V0;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Validator;
 
 class TimelineController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        $posts = Post::with(['prefecture', 'budget', 'target'])->orderBy('id', 'desc')->limit(10)->get();
+        $request = request();
+        $validator = Validator::make($request->all(), [
+            'prefecture_id' => ['bail', 'integer', 'min:1'],
+            'budget_id'     => ['bail', 'integer', 'min:1'],
+            'target_id'     => ['bail', 'integer', 'min:1'],
+        ]);
+        if ($validator->fails()) return response('{}', 400);
+
+        $query = Post::with(['prefecture', 'budget', 'target']);
+        if ($request->prefecture_id) $query->where('prefecture_id', $request->prefecture_id);
+        if ($request->budget_id) $query->where('budget_id', $request->budget_id);
+        if ($request->target_id) $query->where('target_id', $request->target_id);
+        $posts = $query->orderBy('id', 'desc')->limit(10)->get();
         return response($posts, 200);
     }
 }
