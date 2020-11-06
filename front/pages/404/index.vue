4729402eba0b3af061f3704ec2c9a1c638451ae8
takashi0602 <takashioicsn@gmail.com>
2020-11-06 15:57:01 +0900

    add: 404 page, header layout. modify: top page.

---
+++ front/pages/404/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="container center">
+      <p class="msg">このページは存在しません</p>
+      <Button @click.native="$router.push('/')">トップページへ戻る</Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from '~/components/button/index.vue'
+
+export default {
+  components: {
+    Button,
+  },
+  layout: 'header/index',
+}
+</script>
+
+<style lang="scss" scoped>
+.center {
+  text-align: center;
+}
+
+.msg {
+  font-size: 18px;
+  margin: 0 0 40px;
+
+  @include desktop() {
+    margin: 0 0 60px;
+  }
+}
+</style>
