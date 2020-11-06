da83bb65cb956ba7c1ff4084e6cbe51f80cdb1b6
Kazuki Tobita <kazukiti201@gmail.com>
2020-11-06 19:13:46 +0900

    Modify flow chart

--- front/components/flow-chart-renderer/index.vue
+++ front/components/flow-chart-renderer/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <div v-for="node in tree" :key="node.id">
+    <div
+      v-for="node in tree"
+      :key="node.id"
+      :class="[node.type === treeTypes.process ? 'process' : 'edge']"
+    >
       <div>{{ node.name }}</div>
       <button type="button">+</button>
       <div v-if="node.hasOwnProperty('children') && node.children.length > 0">
@@ -24,5 +28,29 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      treeTypes: {
+        root: 1,
+        begin: 2,
+        end: 3,
+        process: 4,
+        if: 5,
+      },
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.edge {
+  border: 2px solid #000;
+  background-color: red;
+}
+
+.process {
+  border: 2px solid #000;
+  background-color: blue;
+}
+</style>
+>
