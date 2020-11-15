b9651d5f0398cfd04865cdbe08c1c8d9686219d7
Kazuki Tobita <kazukiti201@gmail.com>
2020-11-15 11:01:23 +0900

    Modify index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <title>Pripla API</title>
-        <link rel="shortcut icon" href="{{ asset('/favicon.ico', config('app.env') === 'production' ? true : false) }}" />
+        <link rel="shortcut icon" href="{{ asset('/favicon.ico', config('app.env') === 'production') }}" />
     </head>
     <body>
         <h1>Pripla API</h1>
